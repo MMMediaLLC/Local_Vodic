76ea49e96f03e55323f8c102832f25b429182664
MMMediaLLC <mmmediammxvi@gmail.com>
2026-05-29 14:00:43 +0200

    tweak: replace header logo image with gostivarpress.mk/lg.jpg

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="w-full sm:sticky sm:top-0 sm:z-50">
       <Link to="/" className="block">
         <img
-          src="https://i.ibb.co/XGVBhgB/1.png"
+          src="https://gostivarpress.mk/wp-content/uploads/2026/05/lg.jpg"
           alt="GPRESS Local Guide"
           className="w-full h-auto sm:h-28 sm:object-cover sm:object-left block"
         />
